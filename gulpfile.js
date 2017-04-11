cf98f9c065a991e5dd3d7ea85a89073496edde34
ValeriiSergheiev <Val.Serheiev@gmail.com>
2017-04-11 08:11:11 +0300

    advert-details vs advert-info

--- gulpfile.js
+++ gulpfile.js
@@ -123,7 +123,7 @@ gulp.task('serve', () => {
 gulp.task('serve:dist', ['default'], () => {
   browserSync.init({
     notify: false,
-    tunnel: true,
+    //tunnel: true,
     port: 9000,
     server: {
       baseDir: ['dist']
